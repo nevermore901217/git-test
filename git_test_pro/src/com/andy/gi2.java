57868c1e3a059273b3a7235f81c252ebeb34e496
andy <945677674@qq.com>
2021-06-06 12:34:44 +0800

    第一次添加到

---
+++ git_test_pro/src/com/andy/gi2.java
@@ -0,0 +1,7 @@
+package com.andy;
+
+public class gi2 {
+    public static void main(String[] args) {
+        System.out.println("gi2");
+    }
+}
---
+++ git_test_pro/src/com/andy/git.java
@@ -0,0 +1,15 @@
+package com.andy;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git5");
+    }
+    public void run(){
+        System.out.println("run");
+    }
+}
